083e831821e1b1d3e48abea222fe4c22aba317d6
RobinSev <robinseverac@hotmail.fr>
2022-07-04 15:58:05 +0200

    NewScript2

--- script.js
+++ script.js
@@ -3,8 +3,5 @@ const date = new Date();
 const strYear = '' + date.getFullYear();
 const strMonth = '' + (date.getMonth() + 1);
 const strDays = '' + date.getDate();
-const saucisson = 'lol';
-
-throw "PROBLEME";
 
 console.log("DATE NEW = " + strDays + '/' + strMonth + '/' + strYear);
